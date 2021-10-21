ed82b07e31adf57a62b58a22d5f075cb9ff277c1
tt2442 <theokdo@gmail.com>
2021-10-21 21:37:43 +0200

    Feat(install)

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 docker-compose down
 docker-compose up -d
-docker exec slcp bash -c "yarn install --force"
-docker exec slcp bash -c "yarn build"
-docker exec slcp bash -c "composer install"
-docker exec slcp bash -c "npm install --force"
-docker exec slcp bash -c "yarn build"
-docker exec slcp bash -c "symfony console m:migr"
-docker exec slcp bash -c "symfony console d:m:m"
-docker exec slcp bash -c "yes Y | symfony console d:f:l"
+docker exec mediatheque_studi bash -c "yarn install --force"
+docker exec mediatheque_studi bash -c "yarn build"
+docker exec mediatheque_studi bash -c "composer install"
+docker exec mediatheque_studi bash -c "npm install --force"
+docker exec mediatheque_studi bash -c "yarn build"
+docker exec mediatheque_studi bash -c "symfony console m:migr"
+docker exec mediatheque_studi bash -c "symfony console d:m:m"
+docker exec mediatheque_studi bash -c "yes Y | symfony console d:f:l"
